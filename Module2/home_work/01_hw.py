512ffd91e0698785286a43a0845b4af4a5dbdb7f
IvanK11 <102464269+IvanK11@users.noreply.github.com>
2022-03-29 21:33:22 +0300

    Update 01_hw.py

--- Module2/home_work/01_hw.py
+++ Module2/home_work/01_hw.py
@@ -5,4 +5,11 @@
 # Формат выходных данных
 # Выведите «YES», если можно отломить от шоколадки ровно k долек, и «NO» иначе.
 
-# TODO: your code here
+n = int(input("длина: "))
+m = int(input("ширина: "))
+k = int(input("k: "))
+
+if (k%n == 0 or k%m == 0) and k < n*m:
+    print("YES")
+else:
+    print("NO")
