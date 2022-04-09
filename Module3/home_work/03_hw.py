364cdb4edc06dcaf4aba5188d16b8cf134fd9899
IvanK11 <102464269+IvanK11@users.noreply.github.com>
2022-04-09 21:22:26 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,13 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+the_list = []
+num = 10
+i = 1
+while i <= num:
+    the_list.append (random.randint(-100, 100))
+    i += 1
+for el in the_list:
+    if el > 2 and el % 2 == 0:
+        print(el)
