6bf5efa663cd9e85c893760a3d948dbbb4d34ee0
IvanK11 <102464269+IvanK11@users.noreply.github.com>
2022-04-16 19:42:42 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -32,14 +32,44 @@ items = [
     },
 ]
 # Найдите:
-print("Товары на складе представлены брэндами: ")
+# TODO: your code here
+brands = []
+brandlist = []
+for el in items:
+    brands.append(el["brand"])
+for br in brands:
+    brandlist = list(set(brands))
+print("Товары на складе представлены брэндами: ", ", ".join(brandlist))
 
 # TODO: your code here
 
-print("На складе больше всего товаров брэнда(ов): ")
+brands = []
+pairs = []
+max = 0
+the_list = []
+for el in items:
+    brands.append(el["brand"])
+for br in set(brands):
+    pairs.append((br, brands.count(br)))
+for p in pairs:
+    if p[1]>max:
+        max = p[1]
+for pair in pairs:
+    if pair[1] == max:
+        the_list.append(pair[0])
+
+print("На складе больше всего товаров брэнда(ов): ", ", ".join(the_list))
 
 # TODO: your code here
+
+max = 0
+max_el = {}
+for el in items:
+    if el["price"] >= max:
+        max = el["price"]
+        max_el.update(el)
+print (max_el)
+
+#как вкладывать элементы словаря в словарь (т.е. словарь в словарь), чтобы предыдущие записи не затерались?
 
 print("На складе самый дорогой товар брэнда(ов): ")
-
-# TODO: your code here
