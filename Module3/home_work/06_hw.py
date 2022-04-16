e81defe079484ee9d9eac5cde603fa0b65b85459
IvanK11 <102464269+IvanK11@users.noreply.github.com>
2022-04-16 19:55:42 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -31,15 +31,49 @@ items = [
         "price": 1700
     },
 ]
-# Найдите:
-print("Товары на складе представлены брэндами: ")
+# TODO: your code here
+brands = []
+brandlist = []
+for el in items:
+    brands.append(el["brand"])
+for br in brands:
+    brandlist = list(set(brands))
+print("Товары на складе представлены брэндами: ", ", ".join(brandlist))
 
 # TODO: your code here
 
-print("На складе больше всего товаров брэнда(ов): ")
+brands = []
+pairs = []
+max = 0
+the_list = []
+for el in items:
+    brands.append(el["brand"])
+for br in set(brands):
+    pairs.append((br, brands.count(br)))
+for p in pairs:
+    if p[1]>max:
+        max = p[1]
+for pair in pairs:
+    if pair[1] == max:
+        the_list.append(pair[0])
+
+print("На складе больше всего товаров брэнда(ов): ", ", ".join(the_list))
 
 # TODO: your code here
 
-print("На складе самый дорогой товар брэнда(ов): ")
+max = 0
+max_el = []
+brands = []
+for el in items:
+    if el["price"] > max:
+        max = el["price"]
+for el in items:
+    if el["price"] == max:
+        max_el.append(el)
+for el in max_el:
+    brands.append(el["brand"])
+print (brands)
 
-# TODO: your code here
+print("На складе самый дорогой товар брэнда(ов): ", ", ".join(brands))
+
+#апдейт, предыдущую версию этого задания можно не смотреть
